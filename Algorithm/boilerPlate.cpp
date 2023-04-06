831773f80b26d35d97d542de41d8c4f42958983a
Tejas Chougale <92290670+Tejas-2107@users.noreply.github.com>
2023-04-06 17:06:40 +0530

    Add files via upload

---
+++ Algorithm/boilerPlate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+#define ho cout << "YES" << endl;
+#define nahi cout << "NO" << endl;
+#define newl cout << endl;
+#define pb push_back
+#define all(v) sort(v.begin(), v.end())
+#define rall(v) sort(v.rbegin(), v.rend())
+#define fast() ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define ffor(i, j, k, in) for (int i = j; i < k; i += in)
+#define mll map<long long, long long>
+#define mii map<int, int>
+#define mci map<char, int>
+#define sp ' '
+using namespace std;
+
+int main()
+{
+
+    fast();
+    int t;
+    // cin >> t;
+    t = 1;
+
+    while (t--)
+    {
+
+        vector<ll> v;
+        mll m;
+    }
+}
