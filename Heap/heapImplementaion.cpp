7124fff68d83f9cdb989cbb21e57534e6a90f70d
Tejas-2107 <tejaschougale2107@gmail.com>
2023-08-28 12:27:13 +0530

    Heap implementation add

---
+++ Heap/heapImplementaion.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Heap
+{
+public:
+    int arr[100];
+    int size;
+    Heap()
+    {
+        arr[0] = -1;
+        size = 0;
+    }
+    void insertIntoHeap(int data)
+    {
+        size = size + 1;
+        int index = size;
+        arr[index] = data;
+        while (index > 1)
+        {
+            int parentIndex = index / 2;
+            if (arr[parentIndex] < arr[index])
+            {
+                swap(arr[parentIndex], arr[index]);
+                index = parentIndex;
+            }
+            else
+            {
+                break;;
+            }
+        }
+    };
+
+    void printHeap()
+    {
+        for (int i = 1; i <= size; ++i)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    };
+};
+
+int main()
+{
+    Heap h;
+    h.insertIntoHeap(50);
+    h.insertIntoHeap(55);
+    h.insertIntoHeap(53);
+    h.insertIntoHeap(52);
+    h.insertIntoHeap(54);
+    h.printHeap();
+}
