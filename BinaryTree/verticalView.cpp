208d3f105003019b9187254317d7e2776ee796f9
Tejas-2107 <tejaschougale2107@gmail.com>
2023-09-08 21:07:28 +0530

    verticalView file update

--- BinaryTree/verticalView.cpp
+++ BinaryTree/verticalView.cpp
@@ -1,45 +1,44 @@
-vector<int> verticalOrder(Node *root)
+class Solution
 {
-
-    // HD level and vertices
-    map<int, map<int, vector<int>>> distance;
-    // node HD level
-    queue<pair<Node *, pair<int, int>>> q;
-    q.push({root, {0, 0}});
-
-    while (!q.empty())
+public:
+    // Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *root)
     {
 
-        Node *node = q.front().first;
-        int hd = q.front().second.first;
-        int level = q.front().second.second;
-        q.pop();
+        // HD level and vertices
+        map<int, vector<int>> distance;
+        // node HD level
+        queue<pair<Node *, int>> q;
+        q.push({root, 0});
 
-        distance[hd][level].push_back(node->data);
+        while (!q.empty())
+        {
 
-        if (node->left)
-        {
-            q.push({node->left, {hd - 1, level + 1}});
-        }
-        if (node->right)
-        {
-            q.push({node->right, {hd + 1, level + 1}});
-        }
-    }
-    vector<int> ans;
+            Node *node = q.front().first;
+            int hd = q.front().second;
+            q.pop();
 
-    for (auto i : distance)
-    {
-        for (auto j : i.second)
-        {
-            for (auto k : j.second)
+            distance[hd].push_back(node->data);
+
+            if (node->left)
             {
-                ans.push_back(k);
+                q.push({node->left, hd - 1});
+            }
+            if (node->right)
+            {
+                q.push({node->right, hd + 1});
             }
         }
-    }
+        vector<int> ans;
 
-    return ans;
-}
-}
-;
+        for (auto i : distance)
+        {
+            for (auto j : i.second)
+            {
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+    }
+};
