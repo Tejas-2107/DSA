af92e1270201a28a98eef15d8a482359ef5d7cb9
Tejas-2107 <tejaschougale2107@gmail.com>
2024-09-02 13:35:13 +0530

    miniDeletionsTomakePalindrome file updated

--- DynamicProgramming/2D DP/subsequnce questions/miniDeletionsTomakePalindrome.cpp
+++ DynamicProgramming/2D DP/subsequnce questions/miniDeletionsTomakePalindrome.cpp
@@ -1,12 +1,11 @@
 // Given a string 'str' of size ‘n’. The task is to remove or delete the minimum number of characters from the string so that the resultant string is a palindrome. Find the minimum number of characters we need to remove.
 // Note: The order of characters should be maintained.
 
-
 class Solution
 {
 public:
-//gives tle 
-    // int solve(vector<vector<int>>&dp, string s, string sRev, int i, int j){
+    // gives tle
+    //  int solve(vector<vector<int>>&dp, string s, string sRev, int i, int j){
 
     //     if( i >= s.length() or j >= sRev.length()){
     //         return 0;
@@ -25,35 +24,24 @@ public:
     //     return dp[i][j];
 
     // }
-
-    int solveTab(string s, string sRev)
+    int minDeletions(string a1, int n)
     {
-        vector<vector<int>> dp(s.length() + 1, vector<int>(s.length() + 1, 0));
-        int n = s.length();
-        for (int i = n - 1; i >= 0; --i)
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        string a2 = a1;
+        reverse(a2.begin(), a2.end());
+        for (int i = 1; i <= n; ++i)
         {
-            for (int j = n - 1; j >= 0; --j)
+            for (int j = 1; j <= n; ++j)
             {
-                if (s[i] == sRev[j])
+                if (a1[i - 1] == a2[j - 1])
                 {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        return dp[0][0];
+        return n - dp[n][n];
     }
-    int minimumNumberOfDeletions(string s)
-    {
-        // code here
-
-        string sRev = s;
-        reverse(sRev.begin(), sRev.end());
-
-        return (s.size() - solveTab(s, sRev));
-    }
-};
